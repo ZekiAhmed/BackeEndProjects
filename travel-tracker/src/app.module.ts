60474c38f2cd1273011291b4127ae154c8d37dc3
zeki ahmed <changezworld3@gmail.com>
2025-08-20 04:33:18 +0300

    auth module almost finished

--- travel-tracker/src/app.module.ts
+++ travel-tracker/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the configuration globally available
+    }),
+    PrismaModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
