2b4749498480bdb1c8cc4d6c87174b0489eceadc
zeki ahmed <changezworld3@gmail.com>
2025-08-22 01:05:27 +0300

    finished

--- file-upload/src/app.module.ts
+++ file-upload/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { FileUploadModule } from './file-upload/file-upload.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the configuration available globally
+    }),
+    FileUploadModule
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
