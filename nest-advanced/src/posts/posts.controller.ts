eec73f77b50a11ec59b9647db1a0da7a7e37d368
zeki ahmed <changezworld3@gmail.com>
2025-08-27 21:37:05 +0300

    post module finished

--- nest-advanced/src/posts/posts.controller.ts
+++ nest-advanced/src/posts/posts.controller.ts
@@ -1,33 +1,41 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { single } from 'rxjs';
-import type { Post as PostInterface } from './interfaces/post.interface';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PostExistsPipe } from './pipes/post-exists.pipe';
+// import type { Post as PostInterface } from './interfaces/post.interface';
+import { Post as PostEntity } from './entities/post.entity';
 
 @Controller('posts')
 export class PostsController {
     constructor(private readonly postService: PostsService) {}
 
+    // @Get()
+    // findAll(@Query('search') search?:string): PostInterface[] {
+    //     const extractAllPosts = this.postService.findAll();
+
+    //     if(search) {
+    //         return extractAllPosts.filter(singlePost =>singlePost.title.toLowerCase().includes(search.toLowerCase()));
+    //     }
+
+    //     return extractAllPosts
+    // }
     @Get()
-    findAll(@Query('search') search?:string): PostInterface[] {
-        const extractAllPosts = this.postService.findAll();
-
-        if(search) {
-            return extractAllPosts.filter(singlePost =>singlePost.title.toLowerCase().includes(search.toLowerCase()));
-        }
-
-        return extractAllPosts
+    async findAll() : Promise<PostEntity[]> {
+        return await this.postService.findAll();
     }
 
     // @Get(':id')
     // findOne(@Param('id', ParseIntPipe) id:number): PostInterface {
     //     return this.postService.findOne(id);
     // }
+    // @Get(':id')
+    // findOne(@Param('id', ParseIntPipe, PostExistsPipe) id:number): PostInterface {
+    //     return this.postService.findOne(id);
+    // }
     @Get(':id')
-    findOne(@Param('id', ParseIntPipe, PostExistsPipe) id:number): PostInterface {
-        return this.postService.findOne(id);
+    async findOne(@Param('id', ParseIntPipe, PostExistsPipe) id:number): Promise<PostEntity> {
+        return await this.postService.findOne(id);
     }
 
     // @Post('create')
@@ -35,30 +43,50 @@ export class PostsController {
     // create(@Body() createPostData: Omit<PostInterface, 'id' | 'createdAt'>): PostInterface {
     //     return this.postService.create(createPostData);
     // }
+//     @Post('create')
+//     @HttpCode(HttpStatus.CREATED)
+//     @UsePipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//     }),
+//   )
+//     create(@Body() createPostData: CreatePostDto): PostInterface {
+//         return this.postService.create(createPostData);
+//     }
     @Post('create')
     @HttpCode(HttpStatus.CREATED)
     @UsePipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-    }),
-  )
-    create(@Body() createPostData: CreatePostDto): PostInterface {
-        return this.postService.create(createPostData);
+        }),
+    )
+    async create(@Body() createPostData: CreatePostDto): Promise<PostEntity> {
+        return await this.postService.create(createPostData);
     }
 
     // @Put(':id')
     // update(@Param('id', ParseIntPipe) id:number, @Body() updatePostData: Partial<Omit<PostInterface, 'id' | 'createdAt'>>): PostInterface {
     //     return this.postService.update(id, updatePostData);
     // }
+    // @Put(':id')
+    // update(@Param('id', ParseIntPipe, PostExistsPipe) id:number, @Body() updatePostData: UpdatePostDto): PostInterface {
+    //     return this.postService.update(id, updatePostData);
+    // }
     @Put(':id')
-    update(@Param('id', ParseIntPipe, PostExistsPipe) id:number, @Body() updatePostData: UpdatePostDto): PostInterface {
-        return this.postService.update(id, updatePostData);
+    async update(@Param('id', ParseIntPipe, PostExistsPipe) id:number, @Body() updatePostData: UpdatePostDto): Promise<PostEntity> {
+        return await this.postService.update(id, updatePostData);
     }
 
+    // @Delete(':id')
+    // @HttpCode(HttpStatus.NO_CONTENT)
+    // remove(@Param('id', ParseIntPipe, PostExistsPipe) id:number): void {
+    //       this.postService.remove(id);
+    // }   
     @Delete(':id')
     @HttpCode(HttpStatus.NO_CONTENT)
-    remove(@Param('id', ParseIntPipe, PostExistsPipe) id:number): void {
-          this.postService.remove(id);
-    }   
+    async remove(@Param('id', ParseIntPipe, PostExistsPipe) id:number): Promise<void> {
+          return await this.postService.remove(id);
+    }
 }
--- nest-advanced/src/posts/posts.service.ts
+++ nest-advanced/src/posts/posts.service.ts
@@ -1,25 +1,50 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Post } from './interfaces/post.interface';
+// import { Post } from './interfaces/post.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './entities/post.entity';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostsService {
-    private posts:Post[] = [
-        {
-            id:1,
-            title:'First',
-            content:'First Post content',
-            authorName:'John Doe',
-            createdAt: new Date()
-        }
-    ]
+    // private posts:Post[] = [
+    //     {
+    //         id:1,
+    //         title:'First',
+    //         content:'First Post content',
+    //         authorName:'John Doe',
+    //         createdAt: new Date()
+    //     }
+    // ]
 
-    findAll(): Post[] {
-        return this.posts;
+
+    constructor(
+        @InjectRepository(Post)
+        private postsRepository: Repository<Post>,
+        // @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    ) {}
+
+    // findAll(): Post[] {
+    //     return this.posts;
+    // }
+    async findAll(): Promise<Post[]> {
+        return await this.postsRepository.find();
     }
 
 
-    findOne(id:number): Post {
-        const singlPost = this.posts.find(post => post.id === id);
+
+    // findOne(id:number): Post {
+    //     const singlPost = this.posts.find(post => post.id === id);
+
+    //     if(!singlPost) {
+    //         throw new NotFoundException(`Post with id ${id} not found`);
+    //     }
+
+    //     return singlPost;
+    // }
+    async findOne(id:number): Promise<Post>  {
+        const singlPost = await this.postsRepository.findOneBy({id});
 
         if(!singlPost) {
             throw new NotFoundException(`Post with id ${id} not found`);
@@ -28,50 +53,83 @@ export class PostsService {
         return singlPost;
     }
 
-    create(createPostData: Omit<Post, 'id' | 'createdAt'>): Post {
-        const newPost: Post = {
-            id: this.getNextId(),
-            ...createPostData,
-            createdAt: new Date()
-        };
+    // create(createPostData: Omit<Post, 'id' | 'createdAt'>): Post {
+    //     const newPost: Post = {
+    //         id: this.getNextId(),
+    //         ...createPostData,
+    //         createdAt: new Date()
+    //     };
 
-        this.posts.push(newPost);
-        return newPost;
+    //     this.posts.push(newPost);
+    //     return newPost;
+
+    // }
+    async create(createPostData: CreatePostDto): Promise<Post> {
+        const newlyCreatedPost = this.postsRepository.create({
+            title: createPostData.title,
+            content: createPostData.content,
+            // authorName: createPostData.authorName
+        });
+
+        
+        return this.postsRepository.save(newlyCreatedPost);
 
     }
 
-    update(id:number, updatePostData: Partial<Omit<Post, 'id' | 'createdAt'>>): Post {
-        const currentPostIndexEdit = this.posts.findIndex(post => post.id === id);
-        if(currentPostIndexEdit === -1) {
-            throw new NotFoundException(`Post with id ${id} not found`);
+    // update(id:number, updatePostData: Partial<Omit<Post, 'id' | 'createdAt'>>): Post {
+    //     const currentPostIndexEdit = this.posts.findIndex(post => post.id === id);
+    //     if(currentPostIndexEdit === -1) {
+    //         throw new NotFoundException(`Post with id ${id} not found`);
+    //     }
+
+    //     const updatedPost = {
+    //         ...this.posts[currentPostIndexEdit],
+    //         ...updatePostData,
+    //         updatedAt: new Date()
+    //     };
+
+    //     this.posts[currentPostIndexEdit] = updatedPost;
+    //     return updatedPost;
+    // }
+    async update(id:number, updatePostData: UpdatePostDto): Promise<Post> {
+        const findPostToUpdate = await this.findOne(id);
+
+        if(updatePostData.title) {
+            findPostToUpdate.title = updatePostData.title;
         }
 
-        const updatedPost = {
-            ...this.posts[currentPostIndexEdit],
-            ...updatePostData,
-            updatedAt: new Date()
-        };
-
-        this.posts[currentPostIndexEdit] = updatedPost;
-        return updatedPost;
-    }
-
-    remove(id:number):{message:string} {
-        const PostIndex = this.posts.findIndex(post => post.id === id);
-
-        if(PostIndex === -1) {
-            throw new NotFoundException(`Post with id ${id} not found`);
+        if(updatePostData.content) {
+            findPostToUpdate.content = updatePostData.content;
         }
 
-        this.posts.splice(PostIndex, 1);
+        // if(updatePostData.authorName) {
+        //     findPostToUpdate.authorName = updatePostData.authorName;
+        // }
 
-        return {message: `Post with id ${id} has been removed.`};
+        return this.postsRepository.save(findPostToUpdate);
+    }
+
+    // remove(id:number):{message:string} {
+    //     const PostIndex = this.posts.findIndex(post => post.id === id);
+
+    //     if(PostIndex === -1) {
+    //         throw new NotFoundException(`Post with id ${id} not found`);
+    //     }
+
+    //     this.posts.splice(PostIndex, 1);
+
+    //     return {message: `Post with id ${id} has been removed.`};
+    // }
+    async remove(id:number):Promise<void> {
+       const findPostToDelete = await this.findOne(id);
+
+       await this.postsRepository.remove(findPostToDelete);
     }
 
 
-    private getNextId(): number {
-        return this.posts.length > 0 ? Math.max(...this.posts.map(post => post.id)) + 1 : 1;
-    }
+    // private getNextId(): number {
+    //     return this.posts.length > 0 ? Math.max(...this.posts.map(post => post.id)) + 1 : 1;
+    // }
 
 
 }
